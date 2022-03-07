d41794cda30ec9991a30a20a5527fc23bd9704fd
Marcos Cerdan <mm.cerdan@gmail.com>
2022-03-07 16:58:35 -0300

    aulas javascript

---
+++ Javascript/aula04/scripts.js
@@ -0,0 +1,15 @@
+
+
+var jogador01 =0;
+var jogador02 =0;
+var placar;
+
+if(jogador01>0){
+    console.log('jogador 01 marcou ponto');
+}else if (jogador02>0){
+    console.log('jogador 02 marcou ponto');
+}else{
+    console.log('Ninguém marcou');
+}
+     
+ 
