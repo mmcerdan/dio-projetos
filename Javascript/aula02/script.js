21cede21c05d8a5bc8b8a73c47c0d5eea59a0b32
Marcos Cerdan <mm.cerdan@gmail.com>
2022-03-07 13:44:42 -0300

    aulas javascript

--- Javascript/aula02/script.js
+++ Javascript/aula02/script.js
@@ -1,5 +1,49 @@
 
-
+/*
 //booleanos
 var VOuF = false;
 console.log(typeof(VOuF));
+
+let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+console.log (array[3])
+
+array.forEach(function(item, index){console.log(item, index))}
+
+array.push('novo item');
+console.log(array);
+
+array.pop();
+console.log(array);
+
+array.shift();
+console.log(array);
+
+array.unshift('inicio');
+console.log(array);
+
+console.log(array.indexOf(true));
+
+array.splice(0, 3);
+console.log(array);
+
+let novoArray = array.slice(0, 3);
+console.log(novoArray);
+
+var xicara = {cor:'azul', tamanho:'p', funcao:tomarCafe()};
+
+let object = {string:'string', number:1, Boolean:true, array: ["array"], objectInterno {objectInterno: 'Objeto Interno'}};
+
+console.log(object);
+console.log(object.number);
+
+var string = object.string;
+console.log(string);
+
+var {Boolean, objectInterno} = object;
+console.log(Boolean, objectInterno);
+*/
+
+
+
+
+
